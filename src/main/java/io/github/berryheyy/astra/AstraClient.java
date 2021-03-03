6c91db40254c07a7f3f841c79ea9464e435f7408
BerryHeyy <berkaykaplanbk@hotmail.com>
2021-03-03 21:17:46 +0100

    Initial commit

---
+++ src/main/java/io/github/berryheyy/astra/AstraClient.java
@@ -0,0 +1,11 @@
+package io.github.berryheyy.astra;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AstraClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
